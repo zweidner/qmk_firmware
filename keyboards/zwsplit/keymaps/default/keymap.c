bf1557da0144e6414228fad85fb6968f36216cc8
Zach Weidner <zweidner@purdue.edu>
2024-01-27 19:51:57 +0000

    [zwsplit] keymap working correctly

--- keyboards/zwsplit/keymaps/default/keymap.c
+++ keyboards/zwsplit/keymaps/default/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,         KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN,    \
   KC_A,    KC_S,    KC_H,    KC_T,    KC_G,         KC_Y,    KC_N,    KC_E,    KC_O,    KC_I, \
   KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,         KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH, \
-                    KC_LCTL, KC_LSFT, KC_SPC,       KC_ENT,  KC_BSPC, RAISE                 \
+                    KC_LCTL, KC_LSFT, KC_SPC,       KC_ENT,  KC_BSPC, MO(RAISE)                 \
 ),
 
 /* Raise
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    \
   KC_TAB,    KC_LEFT, KC_UP, KC_RGHT,   KC_LBRC,      KC_RBRC, KC_MINS, KC_QUOT,  KC_MINS, KC_EQL, \
   KC_GRV, KC_PGUP,  KC_DOWN, KC_PGDN, _______,      _______, _______, _______, KC_TRNS,  KC_BSLS, \
-                    KC_LALT, KC_LSFT, KC_ESC,      KC_RGUI, _______, _______                    \
+                    KC_LALT, MO(LOWER), KC_ESC,      KC_RGUI, _______, _______                    \
 ),
 
 /* Lower
