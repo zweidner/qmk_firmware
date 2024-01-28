6af27b76e08e1b058565250e5e1e207f43e9c660
Zach Weidner <zweidner@purdue.edu>
2024-01-28 01:46:16 +0000

    [zwergodox] minor keymap changes

--- keyboards/zwergodox/keymaps/default/keymap.c
+++ keyboards/zwergodox/keymaps/default/keymap.c
@@ -37,10 +37,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox_pretty(
   // left hand
-  KC_EQL,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_ESC,              KC_RSFT,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,
+  KC_ESC,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_ESC,              KC_RSFT,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,
   KC_TAB,          KC_Q,        KC_D,          KC_R,    KC_W,    KC_B,    KC_LBRC,             KC_RBRC,     KC_J,    KC_F,    KC_U,    KC_P,              KC_SCLN,           KC_RBRC,
   KC_BSPC,         KC_A,        KC_S,          KC_H,    KC_T,    KC_G,                                        KC_Y,    KC_N,    KC_E,    KC_O,    KC_I, KC_QUOT,
-  KC_LSFT,         KC_Z, KC_X,          KC_M,    KC_C,    KC_V,    KC_ESC,                  KC_ESC, KC_K,    KC_L,    KC_COMM, KC_DOT,           KC_SLSH, KC_RSFT,
+  KC_LSFT,         KC_Z, KC_X,          KC_M,    KC_C,    KC_V,    KC_LPRN,                  KC_RPRN, KC_K,    KC_L,    KC_COMM, KC_DOT,           KC_SLSH, KC_RSFT,
   KC_LGUI, KC_GRV,     KC_LALT, KC_LCTL, KC_LSFT,                                              MO(SYMB),   MO(MDIA), KC_RALT, KC_RBRC, KC_RGUI,
                                                            QK_LOCK, TO(GAME),                KC_RALT, KC_RCTL,
                                                                           KC_HOME,                 KC_PGUP,
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // left hand
   VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,     KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_F12,
-  KC_TRNS, KC_TRNS, KC_LEFT,  KC_UP, KC_RIGHT, KC_TRNS,               KC_TRNS, KC_TRNS,    KC_TRNS,    KC_MINUS,    KC_EQL, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_LEFT,  KC_UP, KC_RIGHT, KC_TRNS,               KC_TRNS, KC_TRNS,    KC_QUOT,    KC_MINUS,    KC_EQL, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_DOWN, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,
   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                         KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,
                                                KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [MDIA] = LAYOUT_ergodox_pretty(
   // left hand
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
