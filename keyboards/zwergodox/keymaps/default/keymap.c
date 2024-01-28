d1265b2b4933c8b99a16c5e00452b64aa398531c
Zach Weidner <zweidner@purdue.edu>
2024-01-28 00:33:23 +0000

    [zwergodox] add key locking

--- keyboards/zwergodox/keymaps/default/keymap.c
+++ keyboards/zwergodox/keymaps/default/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_BSPC,         KC_A,        KC_S,          KC_H,    KC_T,    KC_G,                                        KC_Y,    KC_N,    KC_E,    KC_O,    KC_I, KC_QUOT,
   KC_LSFT,         KC_Z, KC_X,          KC_M,    KC_C,    KC_V,    KC_ESC,                  KC_ESC, KC_K,    KC_L,    KC_COMM, KC_DOT,           KC_SLSH, KC_RSFT,
   KC_LGUI, KC_GRV,     KC_LALT, KC_LCTL, KC_LSFT,                                              MO(SYMB),   MO(MDIA), KC_RALT, KC_RBRC, KC_RGUI,
-                                                           KC_LCTL, TO(GAME),                KC_RALT, KC_RCTL,
+                                                           QK_LOCK, TO(GAME),                KC_RALT, KC_RCTL,
                                                                           KC_HOME,                 KC_PGUP,
                                                          KC_SPC, KC_ENT, KC_END,                  KC_PGDN, KC_ENT, KC_BSPC
 ),
