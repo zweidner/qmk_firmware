94692cb9f22f92fd1b6e7167e10425ea0366f850
Zach Weidner <zweidner@purdue.edu>
2024-02-15 14:10:47 +0000

    [zwatreus] update keymap

--- keyboards/zwatreus/keymaps/default/keymap.c
+++ keyboards/zwatreus/keymaps/default/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,                      KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN,
     KC_A,    KC_S,    KC_H,    KC_T,    KC_G,                      KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,
     KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,                      KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH,
-    KC_GRV, KC_LALT,  KC_LCTL, OSM_SFT, KC_SPC,  KC_ESC,  KC_ENT,  KC_BSPC, MO(_RS), MO(_LW), KC_CAPS, KC_LGUI
+    KC_GRV, KC_ESC,  KC_LALT, KC_LCTL, OSM_SFT,  KC_SPC,  KC_ENT,  KC_BSPC, MO(_RS), MO(_LW), KC_CAPS, KC_LGUI
   ),
 
   /* Raised layer (RS)
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_TAB,  KC_LEFT, KC_UP,   KC_RGHT, KC_LBRC,                   KC_RBRC, KC_TRNS ,KC_QUOT, KC_MINS, KC_EQL,
     KC_TRNS, KC_PGUP, KC_DOWN, KC_PGDN, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ),
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_ESC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ),
 
   /* Lower layer (LW)
    * insert  home   up    end    pgup      ||      vol+    F7     F8     F9    F10
